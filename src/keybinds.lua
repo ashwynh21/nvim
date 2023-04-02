fdcde32c4bdffc9fe0ce6a7a3c1d450d3b09cf98
ashwynh21 <ashwynh21@gmail.com>
2023-04-02 17:45:09 +0200

    feat(keybinds): update keybinds using which key

--- src/keybinds.lua
+++ src/keybinds.lua
@@ -1,19 +1,22 @@
-local function map(m, k, v)
-	vim.keymap.set(m, k, v, { silent = true })
-end
+local which = require("which-key")
 
--- [[ My Keys ]]
-map("n", "<C-s>", ":w!<CR>")
-map("n", "<C-q>", ":q<CR>")
+local keymap = {
+  ["<leader>"] = {
+    t = { ":Neotree toggle left<cr>", "Toggle NeoTree" },
+    g = { ":Neotree git_status toggle float<cr>", "Toggle Git Status" },
+    ["["] = { ":bp<cr>", "Previous Tab" },
+    ["]"] = { ":bn<cr>", "Next Tab" }
+  },
+  ["<C-s>"] = { ":w!<cr>", "Save Changes" },
+  ["<C-q>"] = { ":q<cr>", "Close Tab" },
+}
 
--- [[ Nvim Tree ]]
-map("n", "<leader>t", ":Neotree toggle left<CR>")
-map("n", "<leader>g", ":Neotree git_status toggle float<CR>")
-
--- Move to the next/previous buffer
-map("n", "<leader>[", ":bp<CR>")
-map("n", "<leader>]", ":bn<CR>")
-
--- Close all without saving
-map("n", "<S-q>", ":q!<CR>")
+which.register(keymap, {
+		mode = "n",
+		prefix = "",
+		buffer = nil,
+		silent = true,
+		noremap = true,
+		nowait = false,
+})
 
