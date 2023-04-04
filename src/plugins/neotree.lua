476d10f6e53565844d974fff5602e7cdda7598f8
ashwynh21 <ashwynh21@gmail.com>
2023-04-04 09:09:36 +0200

    feat(gitignore): add plugin dir to ignore list

--- src/plugins/neotree.lua
+++ src/plugins/neotree.lua
@@ -1,5 +1,5 @@
 require("neo-tree").setup({
-	close_if_last_window = true,
+	close_if_last_window = false,
 	enable_diagnostics = true,
 	enable_git_status = true,
 	popup_border_style = "rounded",
@@ -10,5 +10,38 @@ require("neo-tree").setup({
 			hide_gitignored = false,
 		},
 	},
-	window = { width = 38 },
+	window = { width = 40 },
+	default_component_configs = {
+		icon = {
+			folder_closed = "",
+			folder_open = "",
+			folder_empty = "ﰊ",
+			default = "*",
+			highlight = "NeoTreeFileIcon",
+		},
+		modified = {
+			symbol = "[+]",
+			highlight = "NeoTreeModified",
+		},
+		name = {
+			trailing_slash = false,
+			use_git_status_colors = true,
+			highlight = "NeoTreeFileName",
+		},
+		git_status = {
+			symbols = {
+				-- Change type
+				added = "✚",
+        modified = "",
+				deleted = "✖",
+				renamed = "",
+				-- Status type
+				untracked = "",
+				ignored = "",
+				unstaged = "",
+				staged = "",
+				conflict = "",
+			},
+		},
+	},
 })
