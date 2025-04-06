573be1d718e7305007b8fa629de62cb8c6046416
ashwynh21 <ashwynh21@gmail.com>
2025-04-06 09:49:51 +0200

    feat(*): re-did the entire config

--- init.lua
+++ init.lua
@@ -1,13 +1,2 @@
---
---  _____________________   ____(_)______ ___
---  __  __ \  _ \  __ \_ | / /_  /__  __ `__ \
---  _  / / /  __/ /_/ /_ |/ /_  / _  / / / / /
---  /_/ /_/\___/\____/_____/ /_/  /_/ /_/ /_/
---
--- Vanilla Config
-require("settings")
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-require("autocmd")
-require("keybinds")
